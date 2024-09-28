77b6057cfaa58bfc884904968e83e1d6367864f8
Binay Timilsina <binay.timilsina@danelfin.com>
2024-09-28 15:21:00 +0200

    Supabase implemented

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+// config/db.ts
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Supabase client
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Export the Supabase client for use in other modules
+export default supabase;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import express, {Request, Response} from 'express';
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import supabase from './config/db'
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!');
