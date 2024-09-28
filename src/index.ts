57e9e67c830f4766cb50b6db7f476f28b9c7b9be
Binay Timilsina <binay.timilsina@danelfin.com>
2024-09-28 13:20:53 +0200

    Ts config is set up

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
