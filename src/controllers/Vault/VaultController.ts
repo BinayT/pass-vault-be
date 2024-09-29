4e988f9162d3d3084dd045a8ba58f994f9ee82c2
Binay Timilsina <binay.timilsina@danelfin.com>
2024-09-29 18:30:44 +0200

    Getting single vault working perfectly

--- src/controllers/Vault/VaultController.ts
+++ src/controllers/Vault/VaultController.ts
@@ -1,21 +1,6 @@
 import { Request, Response } from 'express';
 import supabase from '../../config/db';
 
-export const getAllUserVaults = async (req: Request, res: Response): Promise<void> => {
-    const { user_email } = req.body;
-    const {data: allVault, error} = await supabase
-    .from('vaults')
-    .select('*')
-    .eq('user_email', user_email)
-
-    if (error) {
-        res.status(400).json({ message: 'Something unexpected happened. Please try again later.', error: error });
-        return;
-    }
-
-    res.status(200).json({ data: allVault });
-}
-
 export const registerVault = async (req: Request, res: Response): Promise<void> => {
     const { user_email, website, email, username, password, notes } = req.body;
 
@@ -44,3 +29,33 @@ export const registerVault = async (req: Request, res: Response): Promise<void> 
      }
      res.status(201).json({ message: 'Vault created successfully' });
 };
+
+export const getAllUserVaults = async (req: Request, res: Response): Promise<void> => {
+    const { user_email } = req.body;
+    const {data: allVault, error} = await supabase
+    .from('vaults')
+    .select('*')
+    .eq('user_email', user_email)
+
+    if (error) {
+        res.status(400).json({ message: 'Something unexpected happened. Please try again later.', error: error });
+        return;
+    }
+
+    res.status(200).json({ data: allVault });
+}
+
+export const getSingleVault = async (req: Request, res: Response): Promise<void> => {
+    const { vault_id } = req.body;
+    const {data: valut, error} = await supabase
+    .from('vaults')
+    .select('*')
+    .eq('id', vault_id)
+
+    if (error) {
+        res.status(400).json({ message: 'We are experiencing error opening this vault. Please try again later.', error: error });
+        return;
+    }
+
+    res.status(200).json({ data: valut });
+}
--- src/routes/vaultEntryRoutes.ts
+++ src/routes/vaultEntryRoutes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import {  } from '../controllers/User/UserController';
-import { getAllUserVaults, registerVault } from '../controllers/Vault/VaultController';
+import { getAllUserVaults, registerVault, getSingleVault } from '../controllers/Vault/VaultController';
 
 const router = express.Router();
 
 // Route for getting all user vaults
 router.get('/getallvaults', getAllUserVaults); 
+
+// Route for getting a single vault entry
+router.get('/getvault', getSingleVault); 
 
 // Route for registering a single vault
 router.post('/registervault', registerVault);
